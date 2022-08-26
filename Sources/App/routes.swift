e4b9a97b786f2519e5d292d33bf8bf46719e33af
saidou Nassufou <saidounelson@gmail.com>
2022-08-26 13:32:22 +0000

    test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,6 +5,11 @@ func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
+
+    app.get("hello") { req async -> String in
+        "Hello, world!"
+    }
+    
     // Create new acronyms
     app.post("api", "acronyms") { req -> EventLoopFuture<Acronym> in
           let acronym = try req.content.decode(Acronym.self)
